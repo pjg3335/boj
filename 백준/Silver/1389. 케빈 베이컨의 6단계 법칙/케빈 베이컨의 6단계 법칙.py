add23fd28ef66dafe7cb3d0fd52f6f6f9428553b
pjg3335 <83161713+pjg3335@users.noreply.github.com>
2022-11-11 09:26:43 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 180 ms, Memory: 116188 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -0,0 +1,38 @@
+import sys
+from collections import defaultdict, deque 
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+graph = defaultdict(bool)
+for _ in range(M):
+    s, e = map(int, input().split())
+    graph[(s,e)] = graph[(e,s)] = True
+
+def bfs(s):
+    visited = [False]*(N+1)
+    cnt = 0
+
+    q = deque()
+    q.append((s,0))
+    visited[s] = True
+
+    while q:
+        s, d = q.popleft()
+        for e in range(1, N+1):
+            if not visited[e] and graph[(s,e)]:
+                q.append((e,d+1))
+                visited[e] = True
+                cnt += d+1
+
+    return cnt
+
+max_cnt = 2**31
+ans = -1
+for s in range(1, N+1):
+    cnt = bfs(s)
+    if cnt < max_cnt:
+        max_cnt = cnt
+        ans = s
+
+print(ans)
