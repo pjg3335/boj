d8e4f583e3f86262aee1c35e4a3792f46286d788
pjg3335 <83161713+pjg3335@users.noreply.github.com>
2022-11-12 10:03:07 +0900

    [Silver IV] Title: 듣보잡, Time: 6240 ms, Memory: 42108 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -0,0 +1,11 @@
+N, M = map(int, input().split())
+
+d = set()
+b = set()
+
+for _ in range(N): d.add(input().rstrip())
+for _ in range(M): b.add(input().rstrip())
+
+db = d & b
+print(len(db))
+for dbj in sorted(list(db)): print(dbj)
