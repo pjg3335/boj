c7ce435536b6a70230ea1947ec44fb04529c1256
pjg3335 <83161713+pjg3335@users.noreply.github.com>
2022-12-22 20:32:36 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 164 ms, Memory: 39472 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+N, K = map(int, input().split())
+
+
+def bfs(s):
+    visited_d = [-1]*100_001
+    q = deque()
+    q.append((s,0))
+    visited_d[s] = 0
+
+    cnt = 0
+
+    while q:
+        s, d = q.popleft()
+        if s == K:
+            cnt += 1
+            continue
+        if visited_d[K] != -1 and visited_d[K]<d:
+            continue
+
+        for e in [s-1, s+1, s*2]:
+            if 0<=e<100_001:
+                if visited_d[e] == -1: 
+                    visited_d[e] = d+1              
+                    q.append((e, d+1))
+                elif visited_d[e] == d+1:
+                    q.append((e, d+1))
+    
+    return (visited_d[K], cnt)
+
+d, cnt = bfs(N)
+print(d)
+print(cnt)
